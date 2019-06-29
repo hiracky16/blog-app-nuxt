b63ecc818d2558a8f5f8b34cc45177be564c7a0c
hiracky16 <h.piiice16@gmail.com>
2019-06-29 11:19:58 +0900

    add: test ar!

---
+++ pages/works/ar/test.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    AR
+    <v-flex height="100%">
+      <a-scene embedded>
+        <a-sky></a-sky>
+        <a-entity
+          camera
+          look-controls
+          wasd-controls
+          position="0 1 3"
+          rotation="-15 0 0"
+        ></a-entity>
+        <a-box v-bind:color="color" opacity="0.75" visible="true"></a-box>
+        <a-marker-camera preset="hiro"></a-marker-camera>
+      </a-scene>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        {
+          src: 'https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js'
+        },
+        {
+          src: 'https://aframe.io/releases/0.9.1/aframe.min.js'
+        }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Roboto'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped></style>
